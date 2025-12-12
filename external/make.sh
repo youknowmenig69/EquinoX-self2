99a7364257fdcde9386d265d959fbdc4baffac0c
root <root>
2025-12-12 10:10:22 +0530

    .

--- external/make.sh
+++ external/make.sh
@@ -257,7 +257,7 @@ if $MAGISKBOOT; then
     esac
     MAGISKBOOT_TMP="$(mktemp -d)"
     MAGISKBOOT_CMDS=(
-        curl -L -s -o "magisk.apk" \ "https://github.com/topjohnwu/Magisk/releases/download/v29.0/Magisk-v29.0.apk"
+        "curl -L -s -o magisk.apk https://github.com/topjohnwu/Magisk/releases/download/v29.0/Magisk-v29.0.apk"
         "unzip -q -j \"magisk.apk\" \"lib/$ARCH/libmagiskboot.so\""
         "mv \"libmagiskboot.so\" \"$TOOLS_DIR/bin/magiskboot\""
         "chmod +x \"$TOOLS_DIR/bin/magiskboot\""
