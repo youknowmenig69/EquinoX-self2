a85a601ee5b2a2ef6b77ac27189ffdef471f44d1
root <root>
2025-12-12 16:01:07 +0530

    idk

--- scripts/utils/common_utils.sh
+++ scripts/utils/common_utils.sh
@@ -106,6 +106,46 @@ _GET_PROP_FILES_PATH()
     printf '%s\n' "${FILES[@]}"
 }
 
+# READ_BYTES_AT <file> <offset> <bytes>
+# Reads the desidered amount of bytes from the supplied file.
+READ_BYTES_AT()
+{
+    _CHECK_NON_EMPTY_PARAM "FILE" "$1" || return 1
+    _CHECK_NON_EMPTY_PARAM "OFFSET" "$2" || return 1
+    _CHECK_NON_EMPTY_PARAM "BYTES" "$3" || return 1
+
+    local FILE="$1"
+    local OFFSET="$2"
+    local BYTES="$3"
+
+    if [ ! -f "$FILE" ]; then
+        LOGE "File not found: ${FILE//$SRC_DIR\//}"
+        return 1
+    fi
+
+    local FILE_SIZE
+    FILE_SIZE="$(wc -c "$FILE" | cut -d " " -f 1)"
+    if ! [[ "$OFFSET" =~ ^[+-]?[0-9]+$ ]] || [[ "$OFFSET" -gt "$FILE_SIZE" ]]; then
+        LOGE "Offset value not valid: $OFFSET"
+        return 1
+    fi
+    if ! [[ "$BYTES" =~ ^[+-]?[0-9]+$ ]] || [[ "$BYTES" -gt "$((FILE_SIZE - OFFSET))" ]]; then
+        LOGE "Bytes value not valid: $BYTES"
+        return 1
+    fi
+
+    local READ
+    local LENGTH
+    READ="$(xxd -p -l "$BYTES" --skip "$OFFSET" "$FILE")"
+    LENGTH="${#READ}"
+
+    while [[ "$LENGTH" -gt 0 ]]; do
+        echo -n "${READ:$LENGTH-2:2}"
+        LENGTH="$((LENGTH - 2))"
+    done
+    echo ""
+}
+
 _GET_SELINUX_LABEL()
 {
     _CHECK_NON_EMPTY_PARAM "PARTITION" "$1" || return 1
