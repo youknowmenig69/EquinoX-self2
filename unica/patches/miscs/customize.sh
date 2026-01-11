0f63887658ebda824570450517ca707bb9f58b4b
youknowmenig69 <youknowmexd6969@gmail.com>
2026-01-11 07:07:02 +0530

    Include KernelSU-Next APK
    Added functionality to preload KernelSU-Next APK and update file configurations.

--- unica/patches/miscs/customize.sh
+++ unica/patches/miscs/customize.sh
@@ -2,3 +2,27 @@
 STOCK_PROP="$(GET_PROP "system" "ro.build.display.id")"
 CUSTOM_PROP="EquinoXROM-aniket $(echo -n ${ROM_VERSION} | cut -d "-" -f1)-${ROM_CODENAME} - ${TARGET_CODENAME} [${STOCK_PROP}]"
 SET_PROP "system" "ro.build.display.id" "$CUSTOM_PROP"
+
+# Adding KSUNEXT Manager 
+KERNELSU_MANAGER_APK="https://github.com/KernelSU-Next/KernelSU-Next/releases/download/v3.0.0/KernelSU_Next_v3.0.0_32857-release.apk"
+APK_PATH="system/preload/KernelSU-Next/com.rifsxd.ksunext-mesa==/base.apk"
+
+LOG "- Adding KernelSU-Next.apk to preload apps"
+mkdir -p "$WORK_DIR/system/$(dirname "$APK_PATH")"
+DOWNLOAD_FILE "$KERNELSU_MANAGER_APK" "$WORK_DIR/system/$APK_PATH"
+
+sed -i "/system\/preload/d" "$WORK_DIR/configs/fs_config-system"
+sed -i "/system\/preload/d" "$WORK_DIR/configs/file_context-system"
+while read -r i; do
+    FILE="$(echo -n "$i"| sed "s.$WORK_DIR/system/..")"
+    [[ -d "$i" ]] && echo "$FILE 0 0 755 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-system"
+    [[ -f "$i" ]] && echo "$FILE 0 0 644 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-system"
+    FILE="$(echo -n "$FILE" | sed 's/\./\\./g')"
+    echo "/$FILE u:object_r:system_file:s0" >> "$WORK_DIR/configs/file_context-system"
+done <<< "$(find "$WORK_DIR/system/system/preload")"
+
+rm -f "$WORK_DIR/system/system/etc/vpl_apks_count_list.txt"
+while read -r i; do
+    FILE="$(echo "$i" | sed "s.$WORK_DIR/system..")"
+    echo "$FILE" >> "$WORK_DIR/system/system/etc/vpl_apks_count_list.txt"
+done <<< "$(find "$WORK_DIR/system/system/preload" -name "*.apk" | sort)"
